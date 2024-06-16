8d6020f0c97646bf0fbb81d04a411001ca82dc8a
lilinlyons <lilinlyons@gmail.com>
2024-06-16 14:48:47 +0300

    updated week2 daily challenge 2 with improvements

--- Week2/Day2/DailyChallenge/dailychallenge.py
+++ Week2/Day2/DailyChallenge/dailychallenge.py
@@ -2,41 +2,28 @@
 number = int(input("Please insert a number: "))
 length = int(input("Please insert a length: "))
 
-i=1
-x = 1
+count=1
+multiple = 1
 mutiples_list = [number]
 
-while x < (length - 1):
-    i+=1
+while multiple < (length - 1):
+    count+=1
     x=len(mutiples_list)
-    mutiples_list.append(i * number)
+    mutiples_list.append(count * number)
 
 print(mutiples_list)
 
 
 # Challenge 2
 word = input("Please enter a word with repeat letters: ")
-separate_letters = list(word)
+new_elements = word[0]
+prev_char = word[0]
 
-i = 0
-j = 0
-new_elements=''
-
-while(j<len(word)):
-
-    if( word[i]==word[j] ):
-        j+=1
-
-    elif((word[j]!=word[i]) or (j==len(word)-1)):
-        new_elements+=word[i]
-
-        i=j
-        j+=1
-
-new_elements += word[j-1]
-
+for char in word[1:]:
+    if char != prev_char:
+        new_elements += char
+        prev_char = char
 print(new_elements)
-
 
 
 
