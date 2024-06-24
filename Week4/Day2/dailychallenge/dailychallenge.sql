a4e0dbb425f6ca9d36c2b9ba4b89e1ff9dfed61c
lilinlyons <lilinlyons@gmail.com>
2024-06-24 13:42:40 +0300

    complete

---
+++ Week4/Day2/dailychallenge/dailychallenge.sql
@@ -0,0 +1,45 @@
+-- q1
+--  it will return number 3
+
+-- q2
+--  it will return number 3
+
+
+-- q3
+--  it will return number 2
+
+
+-- q4
+--  it will return number 3
+
+
+CREATE TABLE FirstTab (
+     id integer, 
+     name VARCHAR(10)
+)
+
+INSERT INTO FirstTab VALUES
+(5,'Pawan'),
+(6,'Sharlee'),
+(7,'Krish'),
+(NULL,'Avtaar')
+
+SELECT * FROM FirstTab
+
+CREATE TABLE SecondTab (
+    id integer 
+)
+
+INSERT INTO SecondTab VALUES
+(5),
+(NULL)
+
+
+SELECT * FROM SecondTab
+
+	
+SELECT *
+FROM firsttab AS ft WHERE ft.id NOT IN (SELECT id FROM secondtab WHERE id IS NULL)
+
+
+
