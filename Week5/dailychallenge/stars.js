7f5f41dc6da9cc090f1dd0be7f2282cd01dcbe68
lilinlyons <lilinlyons@gmail.com>
2024-07-01 14:33:53 +0300

    completed daily challenge stars

---
+++ Week5/dailychallenge/stars.js
@@ -0,0 +1,22 @@
+
+var arr = '*'
+
+for (let i=0; i < 6; i++){
+    console.log(arr)
+    arr += "*"
+}
+
+
+let x = 0;
+var newArray = "*"
+while (x <= 1) {
+    console.log(newArray);
+    newArray+= "*"
+    x++;
+    let j = 0;
+    while (j <= 1) {
+        console.log(newArray);
+        newArray+= "*"
+        j++;
+    }
+}
